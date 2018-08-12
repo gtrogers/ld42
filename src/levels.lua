72492c113cd7cac64c48a9e7c427b0cb313eb92c
g <gareth.rogers0@gmail.com>
2018-08-12 13:49:57 +1000

    tweak level 7

--- src/levels.lua
+++ src/levels.lua
@@ -16,16 +16,20 @@ local CYAN   = {0, 255, 255}
 local YELLOW = {255, 255, 0}
 local RED    = {236, 51, 100}
 
+-- Intro levels
 levels[1] = level(
-  'assets/levels/level_1.png', 1, 'Entry Way', CYAN, 2,
+  'assets/levels/level_1.png', 1, 'Entrance', CYAN, 2,
   'Use the arrow keys to move.', true)
 levels[2] = level('assets/levels/level_2.png', 1, 'Corridor', CYAN, 3)
 levels[3] = level(
-  'assets/levels/level_3.png', 1, 'Switch', CYAN, 4,
+  'assets/levels/level_3.png', 1, 'Switches', CYAN, 4,
   'There must be a way to open those gates!'
   )
-levels[4] = level('assets/levels/level_4.png', 1, 'Turret', CYAN, 5)
-levels[5] = level('assets/levels/level_5.png', 1, 'Turret', CYAN, 6)
-levels[6] = level('assets/levels/level_6.png', 1, 'Turret', YELLOW, 1)
+levels[4] = level('assets/levels/level_4.png', 1, 'Turrets', CYAN, 5)
+levels[5] = level('assets/levels/level_5.png', 1, 'Keys', CYAN, 6)
+
+-- Mid levels
+levels[6] = level('assets/levels/level_6.png', 1, 'The drop', YELLOW, 7)
+levels[7] = level('assets/levels/level_7.png', 1, 'The drop', YELLOW, 1)
 
 return levels
